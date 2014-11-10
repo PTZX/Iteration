a78f38004aaab9adad2c922ed3477ceebe0ed03d
41378 <41378@PC004147.coll.lr.local>
2014-11-10 10:29:30 +0000

    For loop and game

--- Ireration Class Rev 2.py
+++ Ireration Class Rev 2.py
@@ -7,5 +7,7 @@ user = int(input("Enter a number: "))
 for count in range(user):
     print ("Hello World")
     
-if user > 9000:
+if user < 9000:
     print("You Aren't Over 9000")
+else:
+    print("Over 9000")
--- Iteration Class Dev 1.py
+++ Iteration Class Dev 1.py
@@ -1,8 +1,12 @@
 ##Paul Njenje
 ##05/11/14
 ##Iteration Class Dev Exercise 1
-n = int(input('Enter an integer >= 0: '))
-fact = 1
-for i in range(2, n + 1):
-    fact = fact * i
-print(str(n) + ' factorial is ' + str(fact))
+
+n = int(input("Enter a number to find its factorial: "))
+factorial = 1
+integer = 2
+while i >= n:
+    factorial = factorial
+    
+
+n * factorial(n-l)
